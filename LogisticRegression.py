6a294f738dce1114a0cf63d263db810e7fa659b0
Ajay <ajay.patel@mail.mcgill.ca>
2019-09-23 16:41:59 -0400

    added Logistic Regression Class
    starting predict function, created sigmoid function

---
+++ LogisticRegression.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib as mat 
+import math
+
+class LogisticRegression: 
+
+    def sigmoid(prediction):
+        return 1/(1+exp(-prediction))
+    def fit(x):
+        print(x)
+
+
+    def predict(x):
+        print(x)
+
+
+    def evaluate_acc(x,y,z):
+        print(x,y,z)
--- Wine.py
+++ Wine.py
@@ -3,21 +3,33 @@ import matplotlib as mat
 import pandas as pd
 
 
+class CleanData:
+
+    data = pd.read_csv("winequality-red.csv", sep=';')
+
+    def wineBinary(d):
+        counter = 0
+        for i in d.iloc[:,-1]:
+            if (float(i) > 6.0):
+                d.iat[counter,-1] = 1
+            else:
+                d.iat[counter,-1] = 0
+
+            counter+=1
+
+        g = len(d.columns)
+        c = 0
+
+        return d
 
 
-data = pd.read_csv("winequality-red.csv", sep=';')
 
-def wineBinary(data):
-    for i in data.iloc[:,-1]:
-        if i > 5:
-            data.iat[i,-1] = 1
-        else:
-            data.iat[i,-1] = 0
+    q = wineBinary(data)
+    print(data)
+ 
 
-    return data
-
-q = wineBinary(data)
-print(q.iloc[0,-1])
+    #print(q.iloc[:,-1].mean()) #calculates mean of a column 
+    #print(q.iloc[:,-1].std()) #calculates mean of a column 
 
 
 
